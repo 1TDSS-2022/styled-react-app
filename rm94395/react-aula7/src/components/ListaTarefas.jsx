6454eeb84864a763d059717f3b0bd3d8d186b088
Samuel01111 <rm94395@fiap.com.br>
2022-09-16 20:47:35 -0300

    funcao addTarefa, state nTarefa, SPREAD

--- rm94395/react-aula7/src/components/ListaTarefas.jsx
+++ rm94395/react-aula7/src/components/ListaTarefas.jsx
@@ -5,7 +5,7 @@ import { useState } from 'react'
 
 export default function ListaTarefas() {
 
-        const[tarefa, setTarefa] = useState([
+        const[tarefas, setTarefa] = useState([
             {
                 titulo: "Lista de Pagamento",
                 setor: "Dep. Vendas",
@@ -15,11 +15,44 @@ export default function ListaTarefas() {
                 titulo: "Lista de Gente",
                 setor: "Dep. Gente",
                 descricao: "Levar os gente pro lado de lá"
+            },
+            {
+                titulo: "Lista de bugs",
+                setor: "Dep. Bugs",
+                descricao: "Levar os bug pro lado de lá"
             }
     ])
+
+    const [nTarefa, setNTarefa] = useState(
+        {
+        "titulo": "",
+        "setor": "",
+        "descricao": ""
+    }
+    )
+
+    const addTarefa = (e) => {
+        e.preventDefault()
+        // const novaTarefa = {
+        //     titulo: "Planilia de Salarios",
+        //     setor: "Dep. Pessoal",
+        //     descricao: "Gerar Planilhas"
+        // }
+        setNTarefa({
+            titulo: "Planilia de Salarios",
+            setor: "Dep. Pessoal",
+            descricao: "Gerar Planilhas"
+            })
+        setTarefa([...tarefas, nTarefa])
+    }
+/*"..." = spread */
+    
+
     return (
         <DivLista>
-            {tarefa.map((tar, i) => (
+
+            <button onClick={addTarefa}>Adicionar</button>
+            {tarefas.map((tar, i) => (
                 <Tarefa key={i}
                 titulo={tar.titulo}
                 setor={tar.setor}
