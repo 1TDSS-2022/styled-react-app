6be30aa73182f1eb31eba7d25a82266e2432824e
kellerlivia <rm94275@fiap.com.br>
2022-09-16 21:15:41 -0300

    form

--- rm94275/react-aula7/src/components/ListaTarefas.jsx
+++ rm94275/react-aula7/src/components/ListaTarefas.jsx
@@ -1,32 +1,76 @@
-import React from "react";
-import Tarefa from "./Tarefa";
-import { DivLista } from "../style/styled";
-import { useState } from "react";
+import React from 'react'
+import Tarefa from './Tarefa'
+import { DivLista } from '../style/styled'
+import { useState } from 'react'
+import FormTarefas from './FormTarefas'
 
-export default function ListaTarefas() {
-  const [tarefa, setTarefa] = useState([
-    {
-      titulo: "Lista de Pagamento",
-      setor: "Dep. Vendas",
-      descricao: "Levar os pagamentos pro lado de lá",
-    },
-    {
-      titulo: "Lista de Gente",
-      setor: "Dep. Gente",
-      descricao: "Levar os gente pro lado de lá",
-    },
-  ]);
+export default function ListaTarefa() {
 
-  return (
-    <DivLista>
-      {tarefa.map((tar, i) => (
-        <Tarefa
-        key={i}
-        titulo={tar.titulo}
-        setor={tar.setor}
-        descricao={tar.descricao}
-        />
+  const [tarefa, setTarefa] = useState([{
+      titulo: 'Lista de pagamento',
+      setor: 'Dep. Vendas',
+      descricao: 'Levantar os valores das vendas'
+  },
+  {
+      titulo: 'Planilha de salários',
+      setor: 'Dep. Pessoal',
+      descricao: 'Gerar planilha',
+  },
+  {
+      titulo: 'Lista de bugs',
+      setor : 'Dep. TI',
+      descricao: 'Apavorar os programadores'
+  }
+  ])
+
+  const [nTarefa,setNTarefa] = useState({
+      "titulo":"",
+      "setor":"",
+      "descricao":""
+  })
+
+
+  const addTarefa = (e)=>{
+      e.preventDefault()
+
+      
+      setNTarefa({
+          "titulo":"",
+          "setor":"",
+          "descricao":""
+      })
+      setTarefa([...tarefa, nTarefa])
+  }
+
+
+  const captura = (e)=>{
+      e.preventDefault()
+
+
+      const {name, value} = e.target
+
+      if(name === "titulo"){
+          setNTarefa({
+              "titulo":value,
+              "setor":nTarefa.setor,
+              "descricao":nTarefa.descricao
+          })  
+      }
+  }
+
+return (
+  <DivLista>
+
+      <FormTarefas
+          digit={captura}
+      />
+
+      {tarefa.map((tar, i)=>(
+          <Tarefa
+              key={i}
+              tarefa={tar}
+              />
       ))}
-    </DivLista>
-  );
+  </DivLista>
+  )
 }
