12f6f1fde00eba258ec83ee77d2dba8fde660bb2
mouragus <gumora18@hotmail.com>
2022-09-30 20:57:14 -0300

    att

--- rm94280/react-aula7/src/components/FormTarefas.jsx
+++ rm94280/react-aula7/src/components/FormTarefas.jsx
@@ -1,2 +1,28 @@
 import React from 'react'
 
+import React from 'react'
+import { DivForm } from './style/styled'
+
+
+export default function FormTarefas(props) {
+
+    return(
+        <DivForm>
+            <form method="post" onSubmit={props.addTarefa}>
+                <div>
+                    <input name='titulo' placeholder='Titulo' 
+                    value={props.tarefa.titulo} onChange={props.digit} />
+                </div>
+                <div>
+                    <input name='setor' placeholder='Setor' 
+                    value={props.tarefa.setor} onChange={props.digit} />
+                </div>
+                <div>
+                    <textarea name='descricao' placeholder='Descricao' 
+                    value={props.tarefa.descricao} onChange={props.digit} />
+                </div>
+                <button type='submit'>Adicionar</button>
+            </form>
+        </DivForm>
+    )
+}
